36ea5bc2ab4dbc10c442f7661bb86b945e79aae7
Tellow Krinkle <etang110@gmail.com>
2016-07-25 21:38:10 -0500

    Upgrade to mysqli
    PHP 7 doesn’t support the old mysql calls anymore…  Time to switch!

--- queries.php
+++ queries.php
@@ -8,36 +8,35 @@ $otu=$_GET["o"];
 $database_name = 'abv'; // Set this to your Database Name
 $database_username = 'geneforce'; // Set this to your MySQL username
 $database_password = 'wqhew528hw'; // Set this to your MySQL password
-$con = mysql_pconnect('localhost',$database_username, $database_password);
+$sql = mysqli_connect('localhost',$database_username, $database_password, $database_name);
 
 
-if (!$con){
-  die('Could not connect: ' . mysql_error());
+if (mysqli_connect_errno()) {
+	die("Connect failed: " . mysqli_connect_error());
 }
-if (!mysql_select_db($database_name, $con)){
-  die('Could not select database: ' . mysql_error());
-}
+
 $list = explode(',', $name);
 foreach ($list as $value) {
-$in_clause[] = "'".  mysql_real_escape_string($value) . "'";
+	$in_clause[] = "'".  $sql->real_escape_string($value) . "'";
 }
 $in_clause = join(',', $in_clause);
 $list2 = explode(',', $otu);
 foreach ($list2 as $value2) {
-$in_clause2[] = "'".  mysql_real_escape_string($value2) . "'";
+	$in_clause2[] = "'".  $sql->real_escape_string($value2) . "'";
 }
 $in_clause2 = join(',', $in_clause2);
+
 if ($otu == "") {
 	if ($name != "") {
 	$qstring = "select Gene as 'source' , OTU as 'target', Subsite as 'type' from short where Gene IN ($in_clause) and Subsite IN ($sub) and P < '$pval'";
-	$result= mysql_query($qstring) or die (mysql_error()); 
+	$result= $sql->query($qstring) or die (mysqli_error($sql)); 
 	}
 	
 }
 elseif ($otu !=""){ 
 	if ($name != "") {
 	$qstring = "select Gene as 'source' , OTU as 'target', Subsite as 'type' from short where Gene IN ($in_clause) and Subsite IN ($sub) and P < '$pval' and OTU IN ($in_clause2)";
-	$result= mysql_query($qstring) or die (mysql_error()); 
+	$result = $sql->query($qstring) or die (mysql_error($sql)); 
 	}
 }
 
@@ -45,14 +44,15 @@ elseif ($otu !=""){
 #$myquery = "select Gene as 'source' , OTU as 'target', Subsite as 'type' from small where Gene IN ('$name')";
 #$query = mysql_query($myquery) or die (mysql_error());
 
-$links= array();
+$links = array();
 
-for ($x = 0; $x < mysql_num_rows($result); $x++) {
-        $links[] = mysql_fetch_assoc($result);
-    }
+while ($row = $result->fetch_assoc()) {
+	array_push($links, $row);
+}
+$result->close();
 
 echo json_encode($links);
-mysql_close($con);
+$sql->close();
 
 
 ?> 
